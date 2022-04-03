30bbfca4bb37f47d94e439ffcba55d4bd6d54ea1
stolas0 <tabaki98@yandex.ru>
2022-04-03 18:51:25 +0300

    Changed level controller

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class Bird : MonoBehaviour
 {
     Vector3 _initialPosition;
-    bool _isBirdWasLaunched;
-    float _timeSittingAround;
-
     [SerializeField] float _launchPower = 500;
 
     private void Awake()
@@ -20,15 +18,6 @@ public class Bird : MonoBehaviour
     {
         GetComponent<LineRenderer>().SetPosition(0, transform.position);
         GetComponent<LineRenderer>().SetPosition(1, _initialPosition);
-
-        if (_isBirdWasLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
-            _timeSittingAround += Time.deltaTime;
-
-        if (Mathf.Abs(transform.position.y) > 6 || Mathf.Abs(transform.position.x) > 18 || _timeSittingAround > 3)
-        {
-            string currentScene = SceneManager.GetActiveScene().name;
-            SceneManager.LoadScene(currentScene);
-        }
     }
 
     private void OnMouseDown()
@@ -43,7 +32,7 @@ public class Bird : MonoBehaviour
         Vector2 directionToInitialPosition = _initialPosition - transform.position;
         GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition*_launchPower);
         GetComponent<Rigidbody2D>().gravityScale = 1;
-        _isBirdWasLaunched = true;
+        LevelController.BirdWasLaunched();
         GetComponent<LineRenderer>().enabled = false;
     }
 
@@ -51,6 +40,5 @@ public class Bird : MonoBehaviour
     {
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(newPosition.x, newPosition.y, 0);
-
     }
 }
--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,25 +1,82 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour
 {
     private Enemy[] _enemies;
+    private Bird _bird;
+    private Canvas finishScreen;
+    static bool _isBirdLaunched;
+    bool _IsPassedtimeSittingAround;
     private static int _nextLevelIndex = 1;
+    string Level;
+    private int MaxLevel;
+
+    private void Awake()
+    {
+        finishScreen = GetComponent<Canvas>();
+        MaxLevel = SceneManager.sceneCountInBuildSettings;
+        //DontDestroyOnLoad(gameObject);
+    }
 
     private void OnEnable()
     {
+        _bird = FindObjectOfType<Bird>();
         _enemies = FindObjectsOfType<Enemy>();
     }
 
     void Update()
     {
+        //if (!SceneManager.GetSceneByName(Level).isLoaded)
+        //    return;
+
+        if (_isBirdLaunched && _bird.GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+            StartCoroutine("Cooldown");
+
+        if (Mathf.Abs(transform.position.y) > 6 || Mathf.Abs(transform.position.x) > 18 || _IsPassedtimeSittingAround)
+            LoadLevel();
+    }
+
+    private void LoadLevel()
+    {
+        _isBirdLaunched = false;
+        _IsPassedtimeSittingAround = false;
+
+        CheckAllEnemiesDie();
+        Level = "Level" + _nextLevelIndex.ToString();
+
+        if (_nextLevelIndex > MaxLevel)
+            FinishGame();
+        else
+            SceneManager.LoadScene(Level);
+    }
+
+    private void FinishGame()
+    {
+        //Instantiate(_FinishGame);
+        //finishScreen.enabled = true;
+        Debug.Log("Thats all folks");
+        SceneManager.LoadScene("Level1");
+    }
+
+    static public void BirdWasLaunched()
+    {
+        _isBirdLaunched = true;
+    }
+
+    private void CheckAllEnemiesDie()
+    {
         foreach (var enemy in _enemies)
+        {
             if (enemy != null)
                 return;
-
+        }
         _nextLevelIndex++;
-        var Level = "Level" + _nextLevelIndex.ToString();
-        SceneManager.LoadScene(Level);
-
+    }
+    private IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(5f);
+        _IsPassedtimeSittingAround = true;
     }
 }
